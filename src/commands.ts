67dca91a4ad434245e479ce4d6337fd2278e7a07
cvet <cvet@tut.by>
2020-02-14 19:26:49 +0300

    Customizable engine commands

--- src/commands.ts
+++ src/commands.ts
@@ -1,45 +1,127 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as process from 'process';
 
+let fonlinePath: string = '';
+let workspacePath: string = 'Workspace';
 const wslShellPath: string = 'C:\\Windows\\System32\\wsl.exe';
 
 interface TerminalEntry {
     label: string;
-    tooltip: string;
     command: string;
+    shellArgs?: string;
 }
 
 export function init(context: vscode.ExtensionContext) {
-    const fileContent = fs.readFileSync(path.join(context.extensionPath, 'resources', 'commands.json'));
-    const jsonTerminals = JSON.parse(fileContent.toString());
+    if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders[0].uri.scheme == 'file')
+        workspacePath = path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, workspacePath);
+    else
+        workspacePath = path.resolve(workspacePath);
+    console.log('workspacePath: ' + workspacePath);
 
-    const terminals = jsonTerminals.map((terminal: { label: string, command: string, shellArgs: string }) => {
-        if (terminal.command) {
-            terminal.shellArgs = `export FO_INSTALL_PACKAGES=0; ${terminal.command}; read -p "Press enter to close terminal..."`;
+    let foPath: string | undefined;
+    if (vscode.workspace.workspaceFolders) {
+        for (const folder of vscode.workspace.workspaceFolders) {
+            if (folder.uri.scheme == 'file') {
+                foPath = vscode.workspace.getConfiguration('fonline', folder).get<string>('path');
+                if (foPath) {
+                    foPath = path.join(folder.uri.fsPath, foPath);
+                    break;
+                }
+            }
         }
-        terminal.command = 'extension.' + terminal.label.substr(0, 1).toLowerCase() + terminal.label.replace(/ /g, '').substr(1);
-        return terminal;
-    });
+    }
+    if (!foPath) {
+        foPath = vscode.workspace.getConfiguration('fonline').get<string>('path');
+    }
+    if (!foPath) {
+        foPath = process.env.FONLINE_PATH;
+        if (foPath)
+            console.log('FONLINE_PATH: ' + foPath);
+    }
+    if (!foPath) {
+        vscode.window.showErrorMessage('FOnline Engine not found', 'Specify path').then((answer?: string) => {
+            if (answer === 'Specify path')
+                vscode.commands.executeCommand('workbench.action.openGlobalSettings');
+        });
+    } else {
+        if (!path.isAbsolute(foPath))
+            foPath = path.resolve(foPath);
+        fonlinePath = foPath;
+        console.log('fonlinePath: ' + fonlinePath);
+    }
 
+    const terminals: TerminalEntry[] = [];
     const tree = vscode.window.createTreeView('terminalManager', { treeDataProvider: new TerminalTree(terminals) });
     context.subscriptions.push(tree);
 
-    for (const terminal of terminals) {
-        const cmd = vscode.commands.registerCommand(terminal.command, function () {
-            const terminalInstance = vscode.window.createTerminal(terminal.label, wslShellPath, terminal.shellArgs);
-            terminalInstance.show();
+    function applyCommands(commandsFileContent: string) {
+        const commandsJson = JSON.parse(commandsFileContent);
+
+        const newTerminals: TerminalEntry[] = commandsJson.map((terminal: any) => {
+            if (terminal.command) {
+                const foRoot = winToWslPath(fonlinePath);
+                const foWorkspace = winToWslPath(workspacePath);
+                const env = `export FO_ROOT=${foRoot}; export FO_WORKSPACE=${foWorkspace}; export FO_INSTALL_PACKAGES=0`;;
+                terminal.shellArgs = `${env}; ${terminal.command}; read -p "Press enter to close terminal..."`;
+            }
+            terminal.command = 'extension.' + terminal.label.substr(0, 1).toLowerCase() + terminal.label.replace(/ /g, '').substr(1);
+            return terminal;
         });
-        context.subscriptions.push(cmd);
+
+        for (const terminal of newTerminals) {
+            console.log('register ' + terminal.command);
+            const cmd = vscode.commands.registerCommand(terminal.command, () => {
+                console.log('run ' + terminal.command);
+                const terminalInstance = vscode.window.createTerminal({
+                    name: terminal.label,
+                    shellPath: wslShellPath,
+                    shellArgs: terminal.shellArgs,
+                    cwd: fonlinePath,
+                    hideFromUser: false
+                });
+                terminalInstance.show();
+            });
+            context.subscriptions.push(cmd);
+        }
+
+        terminals.push(...newTerminals);
+    }
+
+    const fonlineCommandsFilePath = path.join(fonlinePath, 'fonline-commands.json');
+    fs.exists(fonlineCommandsFilePath, (exists: boolean) => {
+        if (exists) {
+            console.log('applyCommands from engine: ' + fonlineCommandsFilePath);
+            applyCommands(fs.readFileSync(fonlineCommandsFilePath).toString());
+        }
+    });
+
+    if (vscode.workspace.workspaceFolders) {
+        for (const folder of vscode.workspace.workspaceFolders) {
+            console.log('check workspace folder ' + folder.name);
+            if (folder.uri.scheme == 'file') {
+                const commandsFilePath = path.join(folder.uri.fsPath, 'fonline-commands.json');
+                if (commandsFilePath != fonlineCommandsFilePath) {
+                    fs.exists(commandsFilePath, (exists: boolean) => {
+                        if (exists) {
+                            console.log('applyCommands from: ' + commandsFilePath);
+                            applyCommands(fs.readFileSync(commandsFilePath).toString());
+                        }
+                    });
+                }
+            }
+        }
     }
 }
 
 export async function execute(label: string, ...shellArgs: string[]): Promise<number> {
     const terminal = vscode.window.createTerminal({
-        "hideFromUser": true,
-        "name": label,
-        "shellPath": wslShellPath,
-        "shellArgs": shellArgs.concat('exit')
+        name: label,
+        shellPath: wslShellPath,
+        shellArgs: shellArgs.concat('exit'),
+        cwd: fonlinePath,
+        hideFromUser: true
     });
 
     function sleep(ms: number): Promise<void> {
@@ -65,4 +147,12 @@ class TerminalTree implements vscode.TreeDataProvider<TerminalEntry> {
             command: { command: element.command, title: element.label }
         }
     }
+}
+
+function winToWslPath(winPath: string): string {
+    let wslPath = winPath;
+    if (wslPath[1] == ':')
+        wslPath = '/mnt/' + wslPath[0].toLowerCase() + wslPath.substr(2);
+    wslPath = wslPath.replace(/\\/g, '/');
+    return wslPath;
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,10 @@
 import * as vscode from 'vscode';
-
-// import * as build from './build';
-import * as fileExplorer from './fileExplorer';
 import * as commands from './commands'
-import * as status from './status'
 
 export function activate(context: vscode.ExtensionContext) {
   try {
     commands.init(context);
-    status.check(context);
+    checkReadiness();
 
     context.subscriptions.push(
       vscode.commands.registerCommand('extension.run', () => {
@@ -28,4 +24,30 @@ export function activate(context: vscode.ExtensionContext) {
 
 export function deactivate() {
   // ...
+}
+
+async function checkReadiness(): Promise<void> {
+  if (await commands.execute('Check WSL') != 0) {
+    const message = 'Windows Subsystem for Linux is not installed (required WSL2 and Ubuntu-18.04 as distro)';
+    vscode.window.showErrorMessage(message, 'WSL Installation Guide').then((value?: string) => {
+      if (value === 'WSL Installation Guide')
+        vscode.env.openExternal(vscode.Uri.parse('https://docs.microsoft.com/en-us/windows/wsl/wsl2-install'));
+    });
+  } else {
+    const workspaceStatus = await commands.execute('BuildTools/check-workspace.sh');
+    if (workspaceStatus != 0) {
+      let message: string;
+      if (workspaceStatus == 10)
+        message = 'Workspace is not created';
+      else if (workspaceStatus == 11)
+        message = 'Workspace is outdated';
+      else
+        message = 'Can\'t determine workspace state';
+
+      vscode.window.showErrorMessage(message, 'Prepare Workspace').then((value?: string) => {
+        if (value === 'Prepare Workspace')
+          vscode.commands.executeCommand('extension.prepareWorkspace');
+      });
+    }
+  }
 }
